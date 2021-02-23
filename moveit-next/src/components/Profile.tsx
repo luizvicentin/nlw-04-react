8370ae2050b7334738429a58c67bdcd546c71240
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-23 15:39:05 -0300

    Day_02 → Updates

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src='https://github.com/luizvicentin.png' alt='Luiz Vicentin' />
+      <div>
+        <strong>Luiz Vicentin</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
