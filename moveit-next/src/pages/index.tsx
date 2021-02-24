3c24fee792a9fc65c5ea156abf7d84f10ac7a41d
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-24 18:33:22 -0300

    Day_03 → Component ChallengeBox Done

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
@@ -20,8 +22,11 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
