c68821ba14d9dc1100fcce4eeed54c195ff38d88
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-25 22:15:44 -0300

    Day_04 → Challenge Completed

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
 import styles from '..styles/components/Profile.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src='https://github.com/luizvicentin.png' alt='Luiz Vicentin' />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Luiz Vicentin</strong>
         <p>
           <img src='icons/level.svg' alt='Level' />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -48,7 +49,24 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(null);
   }
 
-  function completeChallenge() {}
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
 
   return (
     <ChallengesContext.Provider
@@ -61,6 +79,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         levelUp,
         startNewChallenge,
         resetChallenge,
+        completeChallenge,
       }}
     >
       {children}
